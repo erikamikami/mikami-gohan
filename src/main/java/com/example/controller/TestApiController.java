5f85b417c1a140e569986286617b788086e97cee
erikamikami <g.erika3up@gmail.com>
2026-02-11 15:00:18 +0900

    不要なImport削除

--- src/main/java/com/example/controller/TestApiController.java
+++ src/main/java/com/example/controller/TestApiController.java
@@ -3,7 +3,6 @@ package com.example.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.example.service.UuidService;
 
